0ecb40f58f70ffb6be113377567ced7a0e919430
Noe Lomeli <nolomeli@csumb.edu>
2018-01-22 16:54:08 -0800

    This is the tip app final result

--- tipApp/ViewController.swift
+++ tipApp/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +26,24 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    @IBAction func calculateTip(_ sender: Any) {
+        // Array to hold the different percentage values
+        let tipPercentages = [0.18, 0.2, 0.25]
+        
+        // If the user enters an invalid input like "ABC" the value will be set to 0
+        let bill = Double(billField.text!) ?? 0
+        
+        // Multiply the bill value times the percentage amount selected by the user
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        // Display the amount with two decimals
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+    }
 }
 
